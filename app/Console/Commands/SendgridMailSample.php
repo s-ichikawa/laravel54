b068d6ff52b3757635b26d67697eef640447977c
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-03-22 21:15:30 +0900

    sample

--- app/Console/Commands/SendgridMailSample.php
+++ app/Console/Commands/SendgridMailSample.php
@@ -6,6 +6,7 @@ use App\Events\SendMail;
 use App\Mail\SendGridSample;
 use Illuminate\Console\Command;
 use Illuminate\Mail\Message;
+use Sichikawa\LaravelSendgridDriver\SendGridMessage;
 
 class SendgridMailSample extends Command
 {
@@ -40,32 +41,32 @@ class SendgridMailSample extends Command
      */
     public function handle()
     {
-        event(new SendMail());
-        $result = \Mail::to('ichikawa.shingo.0829@gmail.com')->send(new SendGridSample());
-//        \Mail::send('emails.embed_body_variable', [], function (Message $message) {
-//            $message
-//                ->subject('embed subject variable')
-//                ->from('ichikawa.shingo.0829@gmail.com', 's-ichikawa')
-//                ->to([
-//                    'ichikawa.shingo.0829@gmail.com',
-//                ])
-//                ->replyTo('ichikawa.shingo.0829+replyto@gmail.com', 's-ichikawa！')
-//                ->embedData([
-//                    'personalizations' => [
-//                        [
-//                            'substitutions' => [
-//                                ':myname' => 's-ichikawa',
-//                            ],
-//                        ],
-//                    ],
-//                    'template_id'      => config('sendgrid.templates.sample'),
-//                    'asm'              => [
-//                        'group_id' => 5221,
-//                        'groups_to_display' => [
-//                            5221
-//                        ]
-//                    ],
-//                ], 'sendgrid/x-smtpapi');
-//        });
+//        event(new SendMail());
+//        $result = \Mail::to('ichikawa.shingo.0829@gmail.com')->send(new SendGridSample());
+        \Mail::send('emails.embed_body_variable', [], function (Message $message) {
+            $message
+                ->subject('embed subject variable')
+                ->from('ichikawa.shingo.0829@gmail.com', 's-ichikawa')
+                ->to([
+                    'ichikawa.shingo.0829@gmail.com',
+                ])
+                ->replyTo('ichikawa.shingo.0829+replyto@gmail.com', 's-ichikawa！')
+                ->api([
+                    'personalizations' => [
+                        [
+                            'substitutions' => [
+                                ':myname' => 's-ichikawa',
+                            ],
+                        ],
+                    ],
+                    'template_id'      => config('sendgrid.templates.sample'),
+                    'asm'              => [
+                        'group_id' => 5221,
+                        'groups_to_display' => [
+                            5221
+                        ]
+                    ],
+                ], 'sendgrid/x-smtpapi');
+        });
     }
 }
